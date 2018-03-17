461769a52dda38bb9acdf55ccafc2a019ee19300
Mike Haydan <misha.gajdan@gmail.com>
2018-03-17 18:26:53 +0200

    add core data functional

---
+++ Weather Logger/CoreData/Models/WeatherLocalModel/WeatherLocalModel+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherLocalModel+Extensions.swift
+//  Weather Logger
+//
+//  Created by Mike Haydan on 17/03/2018.
+//  Copyright © 2018 Mike Haydan. All rights reserved.
+//
+
+import Foundation
